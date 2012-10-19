158ed3b3047b59ec1dbf4ba386a4cfd9b4a9e1b8
James Duncan <james.duncan.1991@googlemail.com>
2012-10-19 10:47:52 +0100

    Modified logic to use a master list containing sub-lists of swaps.

--- readme.md
+++ readme.md
@@ -2,23 +2,23 @@
 
 **sublime-swap** is a plug-in for the Sublime Text 2 text editor.  It swaps text selections for logical equivalents, such as:
 
-*width* becomes *height*
+- *width* becomes *height*
 
-*left* becomes *right*
+- *left* becomes *right*
 
-*true* becomes *false*
+- *true* becomes *false*
 
 The plug-in also supports numbers:
 
-*-20* becomes *20*
+- *-20* becomes *20*
 
-*50* becomes *-50*
+- *50* becomes *-50*
 
 And tolerates numbers with CSS values:
 
-*-15px* becomes *15px*
+- *-15px* becomes *15px*
 
-*45%* becomes *-45%*
+- *45%* becomes *-45%*
 
 
 
--- swap.py
+++ swap.py
@@ -13,7 +13,7 @@
 
 import sublime, sublime_plugin, re
 
-class js_swapCommand(sublime_plugin.TextCommand):
+class swapCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         for region in self.view.sel():
             if not region.empty():
@@ -21,57 +21,103 @@ class js_swapCommand(sublime_plugin.TextCommand):
 
                 selection = self.view.substr(region)
 
-                # Left and right swaps
-                if selection == "left":
-                    self.view.replace(edit, region, "right")
-                    col = self.view.rowcol(self.view.sel()[0].begin())[1] + 5
-                elif selection == "right":
-                    self.view.replace(edit, region, "left")
-                    col = self.view.rowcol(self.view.sel()[0].begin())[1] + 4
-
-                # Top and bottom swaps
-                elif selection == "top":
-                    self.view.replace(edit, region, "bottom")
-                    col = self.view.rowcol(self.view.sel()[0].begin())[1] + 6
-                elif selection == "bottom":
-                    self.view.replace(edit, region, "top")
-                    col = self.view.rowcol(self.view.sel()[0].begin())[1] + 3
-
-                # X and y swaps
-                elif selection == "x":
-                    self.view.replace(edit, region, "y")
-                    col = self.view.rowcol(self.view.sel()[0].begin())[1] + 1
-                elif selection == "y":
-                    self.view.replace(edit, region, "x")
-                    col = self.view.rowcol(self.view.sel()[0].begin())[1] + 1
-
-                # Width and height swaps
-                elif selection == "width":
-                    self.view.replace(edit, region, "height")
-                    col = self.view.rowcol(self.view.sel()[0].begin())[1] + 6
-                elif selection == "height":
-                    self.view.replace(edit, region, "width")
-                    col = self.view.rowcol(self.view.sel()[0].begin())[1] + 5
-
-                # Regex search for numbers like 50, -90, 30.32, -1.20px, -50%, 10em
-                # Swap -ive numbers for +ive
-                elif re.search('^[-+]?[0-9]\d{0,2}(\.\d{1,2})?(%)|(px)|(em)?$', selection):
-                    if (selection[:1] == "-"):
-                        self.view.replace(edit, region, selection[1:])
-                        col = self.view.rowcol(self.view.sel()[0].begin())[1] + (len(selection)-1)
-                    else:
-                        # Swap + sign for - sign
-                        if (selection[:1] == "+"):
-                            self.view.replace(edit, region, "-"+selection[1:])
-                            col = self.view.rowcol(self.view.sel()[0].begin())[1] + len(selection)
-                        else:
-                            self.view.replace(edit, region, "-"+selection)
-                            col = self.view.rowcol(self.view.sel()[0].begin())[1] + (len(selection)+1)
+                # List of swaps
+                swaps = []
+                swaps.append(["left", "right"])
+                swaps.append(["top", "bottom"])
 
 
-                row = self.view.rowcol(self.view.sel()[0].begin())[0]
-                target = self.view.text_point(row, col)
+                # Loop through array of swap arrays
+                for s in swaps:
 
-                self.view.sel().clear()
-                self.view.sel().add(sublime.Region(target))
+                    # If selection is in the current subarray
+                    if selection in (s):
+
+                        # Get index of selection and replacement
+                        index = s.index(selection)
+                        replacement = s[0] if index == 1 else s[1]
+
+                        # Replace text
+                        self.view.replace(edit, region, replacement)
+
+                        # Set focus row and col
+                        row = self.view.rowcol(self.view.sel()[0].begin())[0]
+                        col = self.view.rowcol(self.view.sel()[0].begin())[1] + (len(replacement))
+                        target = self.view.text_point(row, col)
+
+                        # Clear and add
+                        self.view.sel().clear()
+                        self.view.sel().add(sublime.Region(target))
+
+
+                # # X and y swaps
+                # elif selection == "x":
+                #     self.view.replace(edit, region, "y")
+                #     col = self.view.rowcol(self.view.sel()[0].begin())[1] + 1
+                # elif selection == "y":
+                #     self.view.replace(edit, region, "x")
+                #     col = self.view.rowcol(self.view.sel()[0].begin())[1] + 1
+
+                # # Width and height swaps
+                # elif selection == "width":
+                #     self.view.replace(edit, region, "height")
+                #     col = self.view.rowcol(self.view.sel()[0].begin())[1] + 6
+                # elif selection == "height":
+                #     self.view.replace(edit, region, "width")
+                #     col = self.view.rowcol(self.view.sel()[0].begin())[1] + 5
+
+                # # Get/set
+                # elif selection == "get":
+                #     self.view.replace(edit, region, "set")
+                #     col = self.view.rowcol(self.view.sel()[0].begin())[1] + 3
+                # elif selection == "set":
+                #     self.view.replace(edit, region, "get")
+                #     col = self.view.rowcol(self.view.sel()[0].begin())[1] + 3
+
+                # # show/hide
+                # elif selection == "show":
+                #     self.view.replace(edit, region, "hide")
+                #     col = self.view.rowcol(self.view.sel()[0].begin())[1] + 4
+                # elif selection == "hide":
+                #     self.view.replace(edit, region, "show")
+                #     col = self.view.rowcol(self.view.sel()[0].begin())[1] + 4
+
+                # # min/max
+                # elif selection == "min":
+                #     self.view.replace(edit, region, "max")
+                #     col = self.view.rowcol(self.view.sel()[0].begin())[1] + 3
+                # elif selection == "max":
+                #     self.view.replace(edit, region, "min")
+                #     col = self.view.rowcol(self.view.sel()[0].begin())[1] + 3
+
+                # # true/false
+                # elif selection == "true":
+                #     self.view.replace(edit, region, "false")
+                #     col = self.view.rowcol(self.view.sel()[0].begin())[1] + 5
+                # elif selection == "false":
+                #     self.view.replace(edit, region, "true")
+                #     col = self.view.rowcol(self.view.sel()[0].begin())[1] + 4
+
+                # # inner/outer
+                # elif selection == "inner":
+                #     self.view.replace(edit, region, "outer")
+                #     col = self.view.rowcol(self.view.sel()[0].begin())[1] + 5
+                # elif selection == "outer":
+                #     self.view.replace(edit, region, "inner")
+                #     col = self.view.rowcol(self.view.sel()[0].begin())[1] + 5
+
+                # # Regex search for numbers like 50, -90, 30.32, -1.20px, -50%, 10em
+                # # Swap -ive numbers for +ive
+                # elif re.search('^[-+]?[0-9]\d{0,2}(\.\d{1,2})?(%)|(px)|(em)?$', selection):
+                #     if (selection[:1] == "-"):
+                #         self.view.replace(edit, region, selection[1:])
+                #         col = self.view.rowcol(self.view.sel()[0].begin())[1] + (len(selection)-1)
+                #     else:
+                #         # Swap + sign for - sign
+                #         if (selection[:1] == "+"):
+                #             self.view.replace(edit, region, "-"+selection[1:])
+                #             col = self.view.rowcol(self.view.sel()[0].begin())[1] + len(selection)
+                #         else:
+                #             self.view.replace(edit, region, "-"+selection)
+                #             col = self.view.rowcol(self.view.sel()[0].begin())[1] + (len(selection)+1)
 
